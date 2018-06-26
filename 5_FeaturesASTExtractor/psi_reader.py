c16e621a46ce1a04485b959dae69d98528026904
PrikhodkoStanislav <prikhodko-stanislav@yandex.ru>
2018-06-26 15:17:49 +0300

    Read PSI and run DFS.

---
+++ 5_FeaturesASTExtractor/psi_reader.py
@@ -0,0 +1,27 @@
+import json
+import io
+
+filename = "Application.kt.json"
+
+f = open(filename, 'r')
+nodes = f.read()
+f.close()
+a = json.loads(nodes)
+print(a)
+
+def dfs(a):
+    for node in a:
+        if 'children' in node:
+            print(node)
+            dfs(node['children'])
+
+def childrenNumber(a):
+    result = 0
+    for node in a:
+        if 'children' in node:
+            result += childrenNumber(node['children'])
+        else:
+            result += 1
+    return result
+
+print(childrenNumber(a))
